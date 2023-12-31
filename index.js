3522e3d1ccb7dcc0066696eedb1a029658980edf
PJMantoss <joelptoss@gmail.com>
2023-12-31 20:16:42 +0100

    add .env file

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log(`Server Started at Port ${3000}`)
 });
+
+// MongoDB Compass
+// DL URL - https://downloads.mongodb.com/compass/mongodb-compass-1.41.0-darwin-arm64.dmg
+// Connection String - mongodb+srv://joelptoss:<password>@cluster0.n7azmij.mongodb.net/
