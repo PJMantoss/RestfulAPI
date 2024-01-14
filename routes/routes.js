7d96c21acecf1eea370dfe7678b139095c35ea12
PJMantoss <joelptoss@gmail.com>
2024-01-14 18:35:25 +0100

    update routes - POST method

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// POST Method - Creating data
+router.post('/post', (req, res) => {
+    res.send('Post API');
+})
+
 module.exports = router;
