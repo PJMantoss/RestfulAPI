92e70b719db0523851abb4708fd6d9d4898a237a
PJMantoss <joelptoss@gmail.com>
2024-01-14 18:43:53 +0100

    update routes - UPDATE method

--- routes/routes.js
+++ routes/routes.js
@@ -11,4 +11,13 @@ router.get('/getAll', (req, res) => {
     res.send('Get All API');
 });
 
+// GET Method (by ID) - Reading Data
+router.get('/getOne/:id', (req, res) => {
+    res.send('Get By ID API');
+});
+
+// UPDATE Method (by ID) - Updating Data
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API');
+})
 module.exports = router;
