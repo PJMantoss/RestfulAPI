8a575fe33ff4111f168771df22a50fc02e5c9ee4
PJMantoss <joelptoss@gmail.com>
2024-04-06 10:13:13 +0100

    update routes

--- routes/routes.js
+++ routes/routes.js
@@ -47,10 +47,11 @@ router.post('/post', async (req, res) => {
 // Get All data from the DB
 router.get('/getAll', async (req, res) => {
     try{
-        res.send('Get All API');
+        const data = Model.find();
+        res.json(data);
     }
     catch(err){
-        //
+        res.status(500).json({message: err.message});
     }
 });
 
